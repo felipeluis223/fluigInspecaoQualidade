28d6e2eb32a6acdc39df6faa6471762b84c823f2
felipeluis223 <lf9072962@gmail.com>
2025-06-18 17:16:11 -0300

    fix: laço de repetição sem condição.

--- forms/etapasControleProcesso/events/displayFields.js
+++ forms/etapasControleProcesso/events/displayFields.js
@@ -3,24 +3,24 @@ function displayFields(form,customHTML){
     var user = getValue("WKUser");
     
     // Grupos definidos do processo:
-    // var groups = {
-    //     corte: "grpCorte",
-    //     serraria: "grpSerraria",
-    //     secagem: "grpSecagem",
-    //     supervisor: "grpSupervisor",
-    // }
+    var groups = {
+        corte: "grpCorte",
+        serraria: "grpSerraria",
+        secagem: "grpSecagem",
+        supervisor: "grpSupervisor",
+    }
 
-    // // Consulta os grupos do usuários:
-    // var constraints = [ DatasetFactory.createConstraint("colleagueGroupPK.colleagueId", user, user, ConstraintType.MUST)];
-    // var groupDataset = DatasetFactory.getDataset("colleagueGroup", null, constraints, null);
+    // Consulta os grupos do usuários:
+    var constraints = [ DatasetFactory.createConstraint("colleagueGroupPK.colleagueId", user, user, ConstraintType.MUST)];
+    var groupDataset = DatasetFactory.getDataset("colleagueGroup", null, constraints, null);
     
-    // // Flag de grupos:
-    // var userGroup = []; 
+    // Flag de grupos:
+    var userGroup = []; 
 
-    // for(var i=0; groupDataset.rowsCount; i++){
-    //     var groupId = groupDataset.getValue(i, "colleagueGroupPK.groupId");
-    //     userGroup.push(groupId);
-    // };
+    for(var i=0; i < groupDataset.rowsCount; i++){
+        var groupId = groupDataset.getValue(i, "colleagueGroupPK.groupId");
+        userGroup.push(groupId);
+    };
     
     
     // Preencher data atual
