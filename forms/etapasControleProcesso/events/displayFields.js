ef21178b938e596db27a8d8559f919e28d4e933d
felipeluis223 <lf9072962@gmail.com>
2025-06-18 16:42:20 -0300

    chore: listando e percorrendo os grupos definidos.

--- forms/etapasControleProcesso/events/displayFields.js
+++ forms/etapasControleProcesso/events/displayFields.js
@@ -1,9 +1,27 @@
 function displayFields(form,customHTML){
-    // Temos que ter grupos criados:
-    var grupoCorte = "grpCorte";
-    var grupoSerraria = "grpSerraria";
-    var grupoSecagem = "grpSecagem";
-    var grupoSupervisor = "grpSupervisor";
-
+    // Obtendo o usuário autenticado:
+    var user = getValue("WKUser");
     
+    // Grupos definidos do processo:
+    var groups = {
+        corte: "grpCorte",
+        serraria: "grpSerraria",
+        secagem: "grpSecagem",
+        supervisor: "grpSupervisor",
+    }
+
+    // Consulta os grupos do usuários:
+    var constraints = [ DatasetFactory.createConstraint("colleagueGroupPK.colleagueId", user, user, ConstraintType.MUST)];
+    var groupDataset = DatasetFactory.getDataset("colleagueGroup", null, constraints, null);
+    
+    // Flag de grupos:
+    var userGroup = []; 
+
+    for(var i=0; groupDataset.rowsCount; i++){
+        var groupId = groupDataset.getValue(i, "colleagueGroupPK.groupId");
+        userGroup.push(groupId);
+    };
+    
+
+
 }
