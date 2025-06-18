ad6ecec9b0e6b8d786db3111bb57287b16811a71
felipeluis223 <lf9072962@gmail.com>
2025-06-18 16:28:20 -0300

    feat: aplicando grupos para realizar a validação de ocultar||desabilitar ação.

---
+++ forms/etapasControleProcesso/events/displayFields.js
@@ -0,0 +1,9 @@
+function displayFields(form,customHTML){
+    // Temos que ter grupos criados:
+    var grupoCorte = "grpCorte";
+    var grupoSerraria = "grpSerraria";
+    var grupoSecagem = "grpSecagem";
+    var grupoSupervisor = "grpSupervisor";
+
+    
+}
