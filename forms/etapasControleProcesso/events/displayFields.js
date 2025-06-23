7176e450533738f1c3dff20427f27399adb94fde
felipeluis223 <lf9072962@gmail.com>
2025-06-23 16:29:22 -0300

    chore: desabilitando todos os campos de input do formulário.

--- forms/etapasControleProcesso/events/displayFields.js
+++ forms/etapasControleProcesso/events/displayFields.js
@@ -35,6 +35,66 @@ function displayFields(form, customHTML) {
     form.setValue("batchDate", formatDate);
     form.setEnabled("batchDate", false);
 
+    log.info("======================================================");
+    log.info("Grupos - USUÁRIO: " + user + " -------> " + userGroup);
+    log.info("======================================================");
+
+
+    // solução do problema: Desabilitar todos os campos inputs e habilitar conforme o grupo do usuário:
+
+    // Colaborador Solicitante: 
+    form.setEnabled("idColab", false);
+    form.setEnabled("nameColab", false);
+    form.setEnabled("batchId", false);
+    form.setEnabled("batchDate", false);
+    
+    // Corte:
+    form.setEnabled("afiamento", false);
+    form.setEnabled("epi", false);
+    form.setEnabled("calibracao", false);
+    form.setEnabled("trincas", false);
+    form.setEnabled("nameRespCorte", false);
+    form.setEnabled("corteDate", false);
+    form.setEnabled("radioTypesCorte", false);
+    form.setEnabled("obsCorte", false);
+
+    // Serraria:
+    form.setEnabled("recebimentoToras", false);
+    form.setEnabled("descascamento", false);
+    form.setEnabled("cortePrimario", false);
+    form.setEnabled("corteSecundario", false);
+    form.setEnabled("inspecaoVisual", false);
+    form.setEnabled("secagem", false);
+    form.setEnabled("armazenagem", false);
+    form.setEnabled("documentacao", false);
+    form.setEnabled("nameRespSerraria", false);
+    form.setEnabled("serrariaDate", false);
+    form.setEnabled("radioTypesSerraria", false);
+    form.setEnabled("obsSerraria", false);
+
+    // Secagem:
+    form.setEnabled("empilhamento", false);
+    form.setEnabled("entradaForno", false);
+    form.setEnabled("curvaSecagem", false);
+    form.setEnabled("verifUmidade", false);
+    form.setEnabled("ausenciaRachaduras", false);
+    form.setEnabled("resfriamento", false);
+    form.setEnabled("nameRespSecagem", false);
+    form.setEnabled("secagemDate", false);
+    form.setEnabled("radioTypesSecagem", false);
+    form.setEnabled("obsSecagem", false);
+
+    // Supervisor:
+    form.setEnabled("idSupervisor", false);
+    form.setEnabled("nameSupervisor", false);
+    form.setEnabled("radioTypesSupervisor", false);
+    form.setEnabled("obsSupervisor", false);
+
+    // Colaborador Solicitante:
+    form.setEnabled("visto", false);
+
+
+
     // Lebrando que estou trabalhando por grupo:
     // No cenário de colaborador (solicitante): Temos que deixar apenas IdColab e IdLote para preencher, e os demais ficar oculto
     // No cenário de corte - temos que deixar os campos do colaborador desabilitados e liberar os campos de "dadosCorte" para o preenchimento
