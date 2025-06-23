1b11c7ed7a46678a3817a609014902f899cf51bd
felipeluis223 <lf9072962@gmail.com>
2025-06-23 18:04:10 -0300

    chore: validando os campos input do formulário.

--- forms/etapasControleProcesso/events/displayFields.js
+++ forms/etapasControleProcesso/events/displayFields.js
@@ -1,51 +1,38 @@
 function displayFields(form, customHTML) {
     var user = getValue("WKUser");
-    var atividade = getValue("WKNumState");
 
-    // Grupos do processo
     var groups = [
         "grpColaborador",
         "grpCorte",
-        "grpSerraria",
         "grpSecagem",
+        "grpSerraria",
         "grpSupervisor"
     ];
 
-    // Dataset de grupos do usuário
     var constraints = [
         DatasetFactory.createConstraint("colleagueGroupPK.colleagueId", user, user, ConstraintType.MUST)
     ];
     var groupDataset = DatasetFactory.getDataset("colleagueGroup", null, constraints, null);
 
-    // var userGroup = [];
-    var userGroup = {}
+    var userGroup = {};
     for (var i = 0; i < groupDataset.rowsCount; i++) {
         var groupId = groupDataset.getValue(i, "colleagueGroupPK.groupId");
-        
-        for(var index = 0; index <= groups.length; index++){
-            if (groupId == groups[index]){
-                userGroup[groups[index]] = groupId
+        for (var index = 0; index < groups.length; index++) {
+            if (groupId == groups[index]) {
+                userGroup[groups[index]] = groupId;
             }
         }
     }
 
-    // Preenche campos iniciais
     var now = new Date();
     var day = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
     var month = now.getMonth() + 1 < 10 ? "0" + (now.getMonth() + 1) : now.getMonth() + 1;
     var year = now.getFullYear();
     var formatDate = day + '/' + month + '/' + year;
 
-    form.setValue("nameColab", user);
-    form.setEnabled("nameColab", false);
-
-    form.setValue("batchDate", formatDate);
-    form.setEnabled("batchDate", false);
 
     log.info("======================================================");
-    log.info("Grupos - USUÁRIO: " + user + " -------> " + userGroup);
-    log.info("======================================================");
-    
+    log.info("Grupos - USUÁRIO: " + user);
     for (var key in userGroup) {
         if (userGroup.hasOwnProperty(key)) {
             log.info("Grupo identificado: " + key + " => " + userGroup[key]);
@@ -53,66 +40,87 @@ function displayFields(form, customHTML) {
     }
     log.info("======================================================");
 
+    // Desabilita todos os campos por padrão
+    var allFields = [
+        "idColab", "nameColab", "batchId", "batchDate",
+        "afiamento", "epi", "calibracao", "trincas", "nameRespCorte", "corteDate", "radioTypesCorte", "obsCorte",
+        "recebimentoToras", "descascamento", "cortePrimario", "corteSecundario", "inspecaoVisual", "secagem",
+        "armazenagem", "documentacao", "nameRespSerraria", "serrariaDate", "radioTypesSerraria", "obsSerraria",
+        "empilhamento", "entradaForno", "curvaSecagem", "verifUmidade", "ausenciaRachaduras", "resfriamento",
+        "nameRespSecagem", "secagemDate", "radioTypesSecagem", "obsSecagem",
+        "idSupervisor", "nameSupervisor", "radioTypesSupervisor", "obsSupervisor",
+        "visto"
+    ];
+    for (var i = 0; i < allFields.length; i++) {
+        form.setEnabled(allFields[i], false);
+    }
 
-    // solução do problema: Desabilitar todos os campos inputs e habilitar conforme o grupo do usuário:
+    // Agora sim: habilita conforme o grupo
+    if ("grpColaborador" in userGroup) {
+        user = getValue("WKUser");
+        
+        form.setValue("nameColab", user);
+        form.setValue("batchDate", formatDate);
 
-    // Colaborador Solicitante: 
-    form.setEnabled("idColab", false);
-    form.setEnabled("nameColab", false);
-    form.setEnabled("batchId", false);
-    form.setEnabled("batchDate", false);
-    
-    // Corte:
-    form.setEnabled("afiamento", false);
-    form.setEnabled("epi", false);
-    form.setEnabled("calibracao", false);
-    form.setEnabled("trincas", false);
-    form.setEnabled("nameRespCorte", false);
-    form.setEnabled("corteDate", false);
-    form.setEnabled("radioTypesCorte", false);
-    form.setEnabled("obsCorte", false);
+        form.setEnabled("nameColab", false);
+        form.setEnabled("batchDate", false);
+        form.setEnabled("idColab", true);
+        form.setEnabled("batchId", true);
+        
+        // Validando a resposta do supervisor para liberar o campo de visto:
+        if((form.getValue("radioTypesSupervisor") == "success") || form.getValue("radioTypesSupervisor") == "danger"){
+            form.setEnabled("visto", false);
+        }
+        form.setEnabled("visto", true);
+    }
 
-    // Serraria:
-    form.setEnabled("recebimentoToras", false);
-    form.setEnabled("descascamento", false);
-    form.setEnabled("cortePrimario", false);
-    form.setEnabled("corteSecundario", false);
-    form.setEnabled("inspecaoVisual", false);
-    form.setEnabled("secagem", false);
-    form.setEnabled("armazenagem", false);
-    form.setEnabled("documentacao", false);
-    form.setEnabled("nameRespSerraria", false);
-    form.setEnabled("serrariaDate", false);
-    form.setEnabled("radioTypesSerraria", false);
-    form.setEnabled("obsSerraria", false);
+    if ("grpCorte" in userGroup) {
+        user = getValue("WKUser");
+        form.setEnabled("afiamento", true);
+        form.setEnabled("epi", true);
+        form.setEnabled("calibracao", true);
+        form.setEnabled("trincas", true);
+        form.setEnabled("nameRespCorte", true);
+        form.setEnabled("corteDate", true);
+        form.setEnabled("radioTypesCorte", true);
+        form.setEnabled("obsCorte", true);
+    }
 
-    // Secagem:
-    form.setEnabled("empilhamento", false);
-    form.setEnabled("entradaForno", false);
-    form.setEnabled("curvaSecagem", false);
-    form.setEnabled("verifUmidade", false);
-    form.setEnabled("ausenciaRachaduras", false);
-    form.setEnabled("resfriamento", false);
-    form.setEnabled("nameRespSecagem", false);
-    form.setEnabled("secagemDate", false);
-    form.setEnabled("radioTypesSecagem", false);
-    form.setEnabled("obsSecagem", false);
+    if ("grpSerraria" in userGroup) {
+        user = getValue("WKUser");
+        form.setEnabled("recebimentoToras", true);
+        form.setEnabled("descascamento", true);
+        form.setEnabled("cortePrimario", true);
+        form.setEnabled("corteSecundario", true);
+        form.setEnabled("inspecaoVisual", true);
+        form.setEnabled("secagem", true);
+        form.setEnabled("armazenagem", true);
+        form.setEnabled("documentacao", true);
+        form.setEnabled("nameRespSerraria", true);
+        form.setEnabled("serrariaDate", true);
+        form.setEnabled("radioTypesSerraria", true);
+        form.setEnabled("obsSerraria", true);
+    }
 
-    // Supervisor:
-    form.setEnabled("idSupervisor", false);
-    form.setEnabled("nameSupervisor", false);
-    form.setEnabled("radioTypesSupervisor", false);
-    form.setEnabled("obsSupervisor", false);
+    if ("grpSecagem" in userGroup) {
+        user = getValue("WKUser");
+        form.setEnabled("empilhamento", true);
+        form.setEnabled("entradaForno", true);
+        form.setEnabled("curvaSecagem", true);
+        form.setEnabled("verifUmidade", true);
+        form.setEnabled("ausenciaRachaduras", true);
+        form.setEnabled("resfriamento", true);
+        form.setEnabled("nameRespSecagem", true);
+        form.setEnabled("secagemDate", true);
+        form.setEnabled("radioTypesSecagem", true);
+        form.setEnabled("obsSecagem", true);
+    }
 
-    // Colaborador Solicitante:
-    form.setEnabled("visto", false);
-
-
-
-
-    // Lebrando que estou trabalhando por grupo:
-    // No cenário de colaborador (solicitante): Temos que deixar apenas IdColab e IdLote para preencher, e os demais ficar oculto
-    // No cenário de corte - temos que deixar os campos do colaborador desabilitados e liberar os campos de "dadosCorte" para o preenchimento
-    // No cenário de serraria - temos que deixar os campos de colaborador e corte desabilitados e liberar os campos de "dadosSerraria" para o preenchimento
-    // Assim por diante em na etapa de Secagem e Supervisor
-} 
+    if ("grpSupervisor" in userGroup) {
+        user = getValue("WKUser");
+        form.setEnabled("idSupervisor", true);
+        form.setEnabled("nameSupervisor", true);
+        form.setEnabled("radioTypesSupervisor", true);
+        form.setEnabled("obsSupervisor", true);
+    }
+}
