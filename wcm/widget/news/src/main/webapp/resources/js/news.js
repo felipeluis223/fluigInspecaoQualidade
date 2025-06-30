8db83ea3d9243cf7a54bc38a27c49738a2f6905b
felipeluis223 <lf9072962@gmail.com>
2025-06-30 16:55:00 -0300

    chore: aplicando 10 itens mockados na tabela.

--- wcm/widget/news/src/main/webapp/resources/js/news.js
+++ wcm/widget/news/src/main/webapp/resources/js/news.js
@@ -9,9 +9,16 @@ var ProcessoDatatable = SuperWidget.extend({
 
     carregarTabela: function () {
         var dados = [
-            { numero: "302", status: "Secagem", data: "2025-06-28" },
-            { numero: "295", status: "Análise", data: "2025-06-25" },
-            { numero: "310", status: "Corte", data: "2025-06-30" }
+            { numero: "1001", etapa: "Corte", data: "2025-06-21" },
+            { numero: "1002", etapa: "Serraria", data: "2025-06-22" },
+            { numero: "1003", etapa: "Secagem", data: "2025-06-23" },
+            { numero: "1004", etapa: "Análise", data: "2025-06-24" },
+            { numero: "1005", etapa: "Corte", data: "2025-06-25" },
+            { numero: "1006", etapa: "Serraria", data: "2025-06-26" },
+            { numero: "1007", etapa: "Secagem", data: "2025-06-27" },
+            { numero: "1008", etapa: "Análise", data: "2025-06-28" },
+            { numero: "1009", etapa: "Corte", data: "2025-06-29" },
+            { numero: "1010", etapa: "Serraria", data: "2025-06-30" }
         ];
 
         var tabela = $("#datatableProcessos");
@@ -20,7 +27,7 @@ var ProcessoDatatable = SuperWidget.extend({
             tabela.append(
                 "<tr>" +
                     "<td>" + item.numero + "</td>" +
-                    "<td>" + item.status + "</td>" +
+                    "<td>" + item.etapa + "</td>" +
                     "<td>" + item.data + "</td>" +
                 "</tr>"
             );
